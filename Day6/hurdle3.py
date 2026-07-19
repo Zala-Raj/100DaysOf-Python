6b5977b2cef174b92a4e32d17caaebf64aa151ab
Zala Raj <zalaraj299@gmail.com>
2026-07-19 11:12:38 +0530

    Added Day6

---
+++ Day6/hurdle3.py
@@ -0,0 +1,33 @@
+'''
+Visit this link : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle3.json
+'''
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear() != True:
+        jump()
+    else:
+        move()
+
+'''
+Or
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
+'''
